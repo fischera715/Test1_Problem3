aebff7e6194ade2ac72a69fdd485bc083719d64e
fischera715 <fischera@sunypoly.edu>
2026-03-11 10:20:21 -0400

    Create dash.py

---
+++ dash.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("Hurricane Maris Relief Operations Dashboard")
+
+infra = pd.read_csv("infrastructure.csv")
+relief = pd.read_csv("relief_deliveries.csv")
